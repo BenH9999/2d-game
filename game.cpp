5270685190decc8b329cc6f5c4eaaf94069b82ea
BenH9999 <ben.houghton7070@outlook.com>
2023-10-31 01:22:57 +0000

    j

--- game.cpp
+++ game.cpp
@@ -8,7 +8,7 @@ void initGame(){
     Font f = LoadFont("font/minecraft.ttf");
     //player newPlayer = {.playerPosition=0};
 
-    newPlayer->playerPosition = {(float)800/2, (float)450/2};
+    newPlayer->playerPosition = {(float)100, (float)370};
     newPlayer->playerSize = {30,30};
     newPlayer->velocity = 0;
 
@@ -16,6 +16,7 @@ void initGame(){
         BeginDrawing();
 
         ClearBackground(GetColor(0xffffffff));
+        DrawRectangleV({50, 400},{700,50},GetColor(0x000000ff));
 
         processGame();
 
@@ -29,19 +30,22 @@ void updateScreen(){
     DrawRectangleV(newPlayer->playerPosition,newPlayer->playerSize,GetColor(0xff0000ff));
 }
 
+int jump_count = 0;
 void processGame(){
     if(IsKeyDown(KEY_RIGHT)) newPlayer->playerPosition.x += 3.0f;
     if(IsKeyDown(KEY_LEFT)) newPlayer->playerPosition.x -= 3.0f;
-    if(IsKeyDown(KEY_UP)&&newPlayer->playerPosition.y >= (float)450/2){
+    if(IsKeyPressed(KEY_UP)&&jump_count < 2){
         newPlayer->velocity = -9.0f;
+        jump_count++;
     }
 
     newPlayer->velocity += 0.4f;
     newPlayer->playerPosition.y += newPlayer->velocity;
 
-    if (newPlayer->playerPosition.y >= (float)450 / 2) {
-        newPlayer->playerPosition.y = (float)450 / 2;
+    if (newPlayer->playerPosition.y >= (float)370) {
+        newPlayer->playerPosition.y = (float)370;
         newPlayer->velocity = 0;
+        jump_count = 0;
     }
 
     //if(IsKeyDown(KEY_UP)) playerPosition.y += 3.0f
